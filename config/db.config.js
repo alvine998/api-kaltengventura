ba61feffe585863a5d9de84405890a143bb72402
alvin <alvinecom2018@gmail.com>
2023-09-13 18:21:58 +0700

    --

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "adminroot0987123",
+    PASSWORD: process.env.DB_PASSWORD || "",
     DB: "kaltengventura",
     dialect: "mysql",
     pool: {
