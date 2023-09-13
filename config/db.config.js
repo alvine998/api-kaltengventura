093e2c095167115ed1f862b248c770a8fd2b2d89
alvin <alvinecom2018@gmail.com>
2023-09-13 18:41:24 +0700

    --

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-    HOST: process.env.DB_HOST || "localhost",
+    HOST: process.env.DB_HOST || "http:gojay.co.id:81",
     USER: "root",
-    PASSWORD: process.env.DB_PASSWORD || "",
+    PASSWORD: process.env.DB_PASSWORD || "adminroot0987123",
     DB: "kaltengventura",
     dialect: "mysql",
     pool: {
