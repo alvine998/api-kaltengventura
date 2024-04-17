e111f078a471eaad7c97a4eb21eacb3dd35aa228
alvin <alvinecom2018@gmail.com>
2024-04-17 16:35:15 +0700

    --

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
 module.exports = {
-    HOST: "103.163.138.104",
-    USER: "bisaprin_alvin",
-    PASSWORD: "alvine1234!",
-    DB: "bisaprin_kaltengventura",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root1234",
+    DB: "kaltengventura",
     dialect: "mysql",
     pool: {
         max: 5,
