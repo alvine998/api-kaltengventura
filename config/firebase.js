a7f3c2657447e474f247d08ae85d560e87d804da
alvin <alvinecom2018@gmail.com>
2024-08-06 16:12:44 +0700

    --

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Adjust the path accordingly
+require("dotenv").config();
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); // Adjust the path accordingly
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
