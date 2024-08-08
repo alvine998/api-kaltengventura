8e7109d3bea2652e631bfaf6c5de4290dd7e564c
alvin <alvinecom2018@gmail.com>
2024-08-08 19:08:45 +0700

    --

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -80,52 +80,7 @@ exports.create = async (req, res) => {
         if (existUsers) {
             return res.status(404).send({ message: "Akun telah terdaftar!" })
         }
-        let uploadPromise = null;
-        if(req.body.photo){
-            const buffers = [
-                { label: "photo", data: Buffer.from(req.body.photo.replace(/^data:image\/\w+;base64,/, ''), 'base64'), raw: req.body.photo },
-            ].filter(v => v !== "undefined");
-    
-            const getDownloadUrl = async (file) => {
-                const [url] = await file.getSignedUrl({
-                    action: 'read',
-                    expires: '03-01-2500', // Set expiration date as needed
-                });
-                return url;
-            };
-    
-           uploadPromise = buffers.map(async (file) => {
-                const { data, label, raw } = file
-                const buffer = Buffer.from(data, 'base64');
-                const storageFile = bucket.file(`uploads/${label}-${req.body.name}`);
-    
-    
-                new Promise((resolve, reject) => {
-                    const stream = storageFile.createWriteStream({
-                        metadata: {
-                            contentType: raw.startsWith('data:image/png') ? 'image/png' : raw.startsWith('data:image/png') ? 'image/jpg' : 'image/jpeg' // Adjust according to your file type
-                        }
-                    });
-    
-                    stream.on('error', (err) => {
-                        console.error(err);
-                        reject('File upload failed.');
-                    });
-    
-                    stream.on('finish', async () => {
-                        await storageFile.makePublic();
-                        const publicUrl = `https://storage.googleapis.com/${bucket.name}/${storageFile.name}`;
-                        resolve(publicUrl);
-                    });
-    
-                    stream.end(buffer);
-                });
-    
-                return await getDownloadUrl(storageFile)
-            })
-        }
-
-        const uploadedFiles = await Promise.all(uploadPromise);
+       
         const payload = {
             ...req.body,
             password: bcrypt.hashSync(req.body.password, 8)
