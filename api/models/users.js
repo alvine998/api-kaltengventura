d77764b803fc9b459436d385830eae996fee69a2
alvine <alvinecom2018@gmail.com>
2026-02-25 16:15:04 +0700

    feat: add fcm_token field to the users model

--- api/models/users.js
+++ api/models/users.js
@@ -1,68 +1,74 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('users', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    photo: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    phone: {
-      type: DataTypes.STRING(13),
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.ENUM('active', 'nonactive'),
-      allowNull: false
-    },
-    from: {
-      type: DataTypes.ENUM('admin', 'apps'),
-      allowNull: false,
-      defaultValue: "apps"
-    },
-    created_on: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    updated_on: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    deleted: {
-      type: DataTypes.TINYINT,
-      allowNull: false,
-      defaultValue: 0
-    }
-  }, {
-    sequelize,
-    tableName: 'users',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+  return sequelize.define(
+    "users",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      photo: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      phone: {
+        type: DataTypes.STRING(13),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("active", "nonactive"),
+        allowNull: false,
+      },
+      from: {
+        type: DataTypes.ENUM("admin", "apps"),
+        allowNull: false,
+        defaultValue: "apps",
+      },
+      created_on: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_on: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      fcm_token: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      deleted: {
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+    },
+    {
+      sequelize,
+      tableName: "users",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "id" }],
+        },
+      ],
+    },
+  );
 };
